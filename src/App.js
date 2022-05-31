8870935545cc0c5c0860177693c1cc80bf3ad6ec
nilsreich <nilsreich@gmail.com>
2022-05-31 19:38:00 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useRef } from "react";
+import io from "socket.io-client";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+import SVGO from "../node_modules/svgo/dist/svgo.browser";
+
+import "./App.css";
+
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState();
+
+  const canvas = useRef();
+
+  function getImage() {
+    canvas.current
+      .exportSvg()
+      .then((data) => {
+        let result = SVGO.optimize(data, {
+          plugins: [
+            { name: "preset-default" }, // enable default preset
+          ],
+        });
+        socket.emit("chat message", result.data);
+        setDrawing(result.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  const [strokewidth, setStrokewidth] = useState(10);
+  const [drawing, setDrawing] = useState();
+
+  useEffect(() => {
+    const newSocket = io(
+      "https://nilsreich-socketio-x5qjg5g36wgv-5000.githubpreview.dev",
+      {
+        withCredentials: true,
+        extraHeaders: {
+          "my-custom-header": "abcd",
+        },
+      }
+    );
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("chat message", function (msg) {
+        setMessage(msg);
+      });
+    }
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <ReactSketchCanvas
+        ref={canvas}
+        width="400px"
+        height="400px"
+        strokeWidth={strokewidth}
+        strokeColor="red"
+        onStroke={() => getImage()}
+      />
+
+      <button onClick={() => setStrokewidth(1)}>Stroke</button>
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: message,
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
